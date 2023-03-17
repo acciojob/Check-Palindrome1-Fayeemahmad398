66a7f156136f06c029e914d67f86cc953df9b2ef
Fayeemahmad398 <117560978+Fayeemahmad398@users.noreply.github.com>
2023-03-17 18:40:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // complete the given function
 
 function palindrome(str){
-var updatestr=str.replace(" ","");
-	updatestr=updatestr.toUpperCase();
+str=str.replace(" ","");
+	str=str.toUpperCase();
 	var i=0;
-	while (i<(updatestr.length/2)) {
-		if(updatestr.charAt(i)!=updatestr.charAt(updatestr.length-1-i))
+	while (i<(str.length/2)) {
+		if(str.charAt(i)!=str.charAt(str.length-1-i))
 		{
 			return false;
 		}
